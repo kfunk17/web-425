7d364e97a4a16a58c8638ea733f608186a8fac07
Karie Funk <kfunk@my365.bellevue.edu>
2020-02-01 22:31:33 -0700

    week-5

---
+++ week-5/funk-md-toolbar/src/app/app.module.ts
@@ -0,0 +1,34 @@
+  
+/*
+============================================
+; Title: Assignment 5.2
+; Author: Karie Funk
+; Date: 01 February 2020
+; Description: Angular Design Materials
+;===========================================
+*/
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from "@angular/material";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
